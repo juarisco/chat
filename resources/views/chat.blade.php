94967e937c0a6e9aff56fcc1859f50269da4cd6d
juarisco <juarisco@outlook.com>
2018-12-28 11:48:50 -0500

    How to Use Vuejs with Laravel

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -7,18 +7,26 @@
     <title>Document</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <style>
+        .list-group{
+            overflow-y: scroll;
+            height: 200px;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
         <div class="row" id="app">
-            <ul class="list-group offset-4 col-4">
-                <li class="list-group-item active">Chat Romm</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Morbi leo risus</li>
-                <li class="list-group-item">Porta ac consectetur ac</li>
-                <li class="list-group-item">Vestibulum at eros</li>
-                <input type="text" name="" id="" class="form-control" placeholder="Type your message here ..">
-            </ul>
+            <div class="offset-4 col-4">
+                <li class="list-group-item active">Chat Room</li>
+                <ul class="list-group">
+                    <message v-for="value in chat.message">
+                        @{{ value }}
+                    </message>
+                </ul>
+                <input type="text" name="" id="" class="form-control" placeholder="Type your message here .." v-model='message' @keyup.enter='send'>
+            </div>
+
         </div>
     </div>
 
